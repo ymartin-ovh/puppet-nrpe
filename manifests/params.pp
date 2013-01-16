0642ad0946634afa0b1e60eb2db2a1a9db0db785
William Van Hevelingen <blkperl@cat.pdx.edu>
2013-01-15 19:31:13 -0800

    Add redhat params to nrpe

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,6 +8,8 @@ class nrpe::params {
 
   case $::osfamily {
     'Debian':  {
+      $nrpe_user        = 'nagios'
+      $nrpe_group       = 'nagios'
       $nrpe_pid_file    = '/var/run/nagios/nrpe.pid'
       $nrpe_config      = '/etc/nagios/nrpe.cfg'
       $nrpe_include_dir = '/etc/nagios/nrpe.d/'
@@ -18,6 +20,8 @@ class nrpe::params {
       ]
     }
     'Solaris': {
+      $nrpe_user        = 'nagios'
+      $nrpe_group       = 'nagios'
       $nrpe_pid_file    = '/var/run/nrpe.pid'
       $nrpe_config      = '/opt/csw/etc/nrpe.cfg'
       $nrpe_include_dir = '/opt/csw/etc/nrpe.d'
@@ -28,6 +32,16 @@ class nrpe::params {
       ]
     }
     'RedHat':  {
+      $nrpe_user        = 'nrpe'
+      $nrpe_group       = 'nrpe'
+      $nrpe_pid_file    = '/var/run/nrpe/nrpe.pid'
+      $nrpe_config      = '/etc/nagios/nrpe.cfg'
+      $nrpe_include_dir = '/etc/nrpe.d'
+      $nrpe_service     = 'nrpe'
+      $nrpe_packages    = [
+        'nrpe',
+        'nagios-plugins-nrpe'
+      ]
     }
     'FreeBSD': {
     }
