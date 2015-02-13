fc69e9e398d5780671adbf078ef0b9b257b12877
Maxime VISONNEAU <maxime.visonneau@gmail.com>
2015-02-13 19:18:40 +0100

    Added sudo support

--- manifests/command.pp
+++ manifests/command.pp
@@ -7,6 +7,7 @@ define nrpe::command (
   $package_name = $nrpe::params::nrpe_packages,
   $service_name = $nrpe::params::nrpe_service,
   $file_group   = $nrpe::params::nrpe_files_group,
+  $sudo         = false,
 ) {
 
   file { "${include_dir}/${title}.cfg":
