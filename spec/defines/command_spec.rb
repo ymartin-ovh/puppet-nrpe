70c9d6f92f256084cee99a30a2acb2602bfeea23
Alexander Fisher <alex@linfratech.co.uk>
2018-10-31 09:08:26 +0000

    Rubocop: Autofix Layout/IndentationWidth

--- spec/defines/command_spec.rb
+++ spec/defines/command_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe 'nrpe::command', type: :define do
-    let(:pre_condition) { 'include nrpe' }
+  let(:pre_condition) { 'include nrpe' }
 
-    let :facts do
-     { osfamily: 'Debian',
-       architecture: 'x86_64'
+  let :facts do
+    { osfamily: 'Debian',
+      architecture: 'x86_64'
+   }
+  end
+
+  let (:title) { 'check_users' }
+  let :params do
+    {
+      command: 'check_users -w 5 -c 10',
+      ensure: 'present'
     }
-    end
+  end
 
-    let (:title) { 'check_users' }
-    let :params do
-      {
-        command: 'check_users -w 5 -c 10',
-        ensure: 'present'
-      }
-    end
-
-    it { is_expected.to compile.with_all_deps }
-    it { should contain_file('/etc/nagios/nrpe.d/check_users.cfg') }
+  it { is_expected.to compile.with_all_deps }
+  it { should contain_file('/etc/nagios/nrpe.d/check_users.cfg') }
 end
